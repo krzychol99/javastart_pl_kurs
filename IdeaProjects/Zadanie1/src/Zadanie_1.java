58e4d5c20dac1843146b9f5c5219901b2a0fe5b4
krzychol99 <krzysztof.odachowski@gmail.com>
2016-12-15 21:35:00 +0100

    Pierwszy commit - zadanie pierwsze

---
+++ IdeaProjects/Zadanie1/src/Zadanie_1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by MałaMi on 2016-12-15.
+ */
+public class Zadanie_1 {
+    public static void main(String[] args) {
+        System.out.println("Uczę \nsię \nprogramowania");
+    }
+}
