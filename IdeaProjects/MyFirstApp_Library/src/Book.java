00df81c5fb38463ed9c8788573f566b6cd70cfed
krzychol99 <krzysztof.odachowski@gmail.com>
2016-12-19 21:38:36 +0100

    Addition of new Book.class

---
+++ IdeaProjects/MyFirstApp_Library/src/Book.java
@@ -0,0 +1,11 @@
+/**
+ * Created by MałaMi on 2016-12-19.
+ */
+public class Book {
+    String title;
+    String author;
+    int releaseDate;
+    int pages;
+    String publisher;
+    String isbn;
+}
--- IdeaProjects/MyFirstApp_Library/src/Library.java
+++ IdeaProjects/MyFirstApp_Library/src/Library.java
@@ -5,20 +5,21 @@ public class Library {
     public static void main(String[] args) {
         final String appName = "Biblioteka v0.1";
 
-        String title = "Java. Podstawy. Wydanie VIII";
-        String author = "Cay S. Horstmann, Gary Cornell";
-        int releaseDate = 2008;
-        int pages = 888;
-        String publisher = "Helion";
-        String isbn = "978-83-246-1478-3";
+        Book book1 = new Book();
+        book1.title = "Java. Podstawy. Wydanie VIII";
+        book1.author = "Cay S. Horstmann, Gary Cornell";
+        book1.releaseDate = 2008;
+        book1.pages = 888;
+        book1.publisher = "Helion";
+        book1.isbn = "978-83-246-1478-3";
 
         System.out.print(appName);
         System.out.println("Książki dostępne w bibliotece:");
-        System.out.println(title);
-        System.out.println(author);
-        System.out.println(releaseDate);
-        System.out.println(pages);
-        System.out.println(publisher);
-        System.out.println(isbn);
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.releaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
     }
 }
