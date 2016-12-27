89cdbc4f8badd6f5b5cfbb53aab9d85d77690703
krzychol99 <krzysztof.odachowski@gmail.com>
2016-12-27 14:09:00 +0100

    Initial commit

---
+++ javastart.pl_kurs/Temat_2_HelloWorld/src/HelloName.java
@@ -0,0 +1,9 @@
+/**
+ * Created by krzychol99 on 2016-12-27.
+ */
+public class HelloName {
+    public static void main(String[] args) {
+
+        System.out.println("Witaj Krzysztof!");
+    }
+}
