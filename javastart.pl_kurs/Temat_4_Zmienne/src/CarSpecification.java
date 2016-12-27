1706d210ef19fb653bf8bb59f7d70c7306a7473d
krzychol99 <krzysztof.odachowski@gmail.com>
2016-12-27 17:06:17 +0100

    Add CarSpecification.class

---
+++ javastart.pl_kurs/Temat_4_Zmienne/src/CarSpecification.java
@@ -0,0 +1,21 @@
+/**
+ * Created by krzychol99 on 2016-12-27.
+ */
+public class CarSpecification {
+    public static void main(String[] args) {
+        String brand = "Seat";
+        String model = "Toledo";
+        String petrolType = "Fuel + LPG";
+        String engineType = "1.6";
+        String color = "brown";
+        double maxSpeed = 205;
+        int productionYear = 2003;
+
+        System.out.println("BRAND: " + brand + "; MODEL: " + model);
+        System.out.println("PRODUCTION YEAR: " + productionYear);
+        System.out.println("ENGINE TYPE: " + engineType);
+        System.out.println("PETROL TYPE: " + petrolType);
+        System.out.println("COLOR: " + color);
+        System.out.println("MAX SPEED: " + maxSpeed + " km/h");
+    }
+}
